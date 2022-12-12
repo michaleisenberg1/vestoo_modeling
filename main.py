b85b02213fc6729da890cd3d8bbca827adbd6cb0
michaleisenberg1 <m.e.pasand@gmail.com>
2022-12-12 04:01:00 +0200

    add readme.md + cosmetic changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import torch
 from statsmodels.tsa.statespace.sarimax import SARIMAX
 import numpy as np
+import pandas as pd
 import scipy.stats as st
 import matplotlib.pyplot as plt
 
@@ -8,9 +9,10 @@ import matplotlib.pyplot as plt
 class Arima(torch.nn.Module):
     def __init__(self):
         super().__init__()
+        self.order = (0, 1, 1)
 
     def forward(self, x, n_samples):
-        model = SARIMAX(x, order=(0, 1, 1), trend='t').fit()
+        model = SARIMAX(x, order=self.order, trend='n').fit()
         return model.get_forecast(n_samples)
 
 
@@ -20,53 +22,39 @@ def generate_time_series(initial_state=2, alpha=0.4, theta=1, nsamples=20):
     for t in range(len(n)):
         y_t = initial_state + alpha * t + theta * np.cumsum(n)[t]
         ts.append(y_t)
-    return np.array(ts)
+    return pd.Series(ts)
 
 
-def plot_arima_prediction(train, test, x_train, x_test, pred):
+def plot_arima_prediction(train, test, pred):
+    x_test = test.index
+    x_train = train.index
+    ci = pred.conf_int()
     plt.plot(x_train, train, '.', label='train')
     plt.plot(x_test, test, '.', label='test')
     plt.plot(x_test, pred.predicted_mean, '.', label='pred')
-    plt.fill_between(x_test, pred.conf_int()[:, 0], pred.conf_int()[:, 1], color='k', alpha=0.15)
+    plt.fill_between(x_test, ci['lower y'], ci['upper y'], color='k', alpha=0.15)
     plt.legend()
     plt.show()
 
 
-def observed_probability(test, arima_forecast_result):
-    z_scores = (test - arima_forecast_result.predicted_mean) / arima_forecast_result.se_mean
-    return np.prod(st.norm.cdf(z_scores))
+def observed_probability(observed, forecast_result):
+    z_scores = (observed - forecast_result.predicted_mean) / forecast_result.se_mean
+    neg_z_scores = [z if z <= 0 else -z for z in z_scores]
+    return np.prod(st.norm.cdf(neg_z_scores) * 2)
 
 
 if __name__ == '__main__':
     ts = generate_time_series()
-    train = ts[:14]
-    test = ts[14:]
-    x_train = np.arange(14)
-    x_test = np.arange(14, 20)
+    train = ts.loc[:13]
+    test = ts.loc[14:]
 
     arima_model = Arima()
     pred = arima_model.forward(train, 6)
-    plot_arima_prediction(train, test, x_train, x_test, pred)
+    plot_arima_prediction(train, test, pred)
 
-    p = observed_probability(test, pred)
-    print('Probability of observing the test set given fitted model:', round(p, 5))
-
-    train = np.append(ts[:7], ts[13:])
-    # train = ts[:7]
-    test = ts[7:13]
-
-    model = SARIMAX(train, order=(0, 1, 1), trend='t').fit()
-    pred = model.predict(7, 12)
-
-    x_train = np.append(np.arange(7), np.arange(13, 20))
-    x_test = np.arange(7, 13)
-    plt.plot(x_train, train, '.', label='train')
-    # plt.plot(x_train, np.append(ts[:7], ts[13:]), '.', label='train')
-    plt.plot(x_test, test, '.', label='test')
-    plt.plot(x_test, pred, '.', label='pred')
-    plt.legend()
-    plt.show()
-
-    # 1.2.2) I would fit the ARIMA model with a Kalman filter instead of maximal likelihood,
-    # which would deal better with the missing values.
-
+    print(train.shape, test.shape)
+    p = observed_probability(test.values, pred)
+    print(
+        'Probability of observing the test set given fitted model:',
+        round(p, 5)
+    )
---
+++ readme.md
@@ -0,0 +1,4 @@
+ARIMA Cross-Validation
+
+1.2.2) I would fit the ARIMA model with a Kalman filter instead of maximal likelihood, 
+which would deal better with the missing values.
